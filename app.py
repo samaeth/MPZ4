0e10a2a27ff84e70a866aae5963c8f93d01623e8
Emeka-Ukandu Samuel <158041987+samaeth@users.noreply.github.com>
2026-04-20 13:17:18 +0100

    Use environment variable for DATABASE_URL
    Replaced hardcoded DATABASE_URL with environment variable.

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from forms import SignUpForm, LoginForm
 import requests
 import psycopg2
 import psycopg2.extras
+import os
 from urllib.parse import quote_plus
 
 app = Flask(__name__)
@@ -11,7 +12,7 @@ app = Flask(__name__)
 API_KEY = '9363b0c3'
 app.secret_key = 'a48a928d5a9f35f11114bba8'
 password = quote_plus('Yvl_S@m@el25')
-DATABASE_URL = f'postgresql://postgres.jpgomwjymnfqrnyhuafv:{password}@aws-0-eu-west-1.pooler.supabase.com:5432/postgres'
+DATABASE_URL = os.environ.get('DATABASE_URL')
 print(DATABASE_URL)
 bcrypt = Bcrypt(app)
 
